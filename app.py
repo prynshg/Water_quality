03a55b0a1f35d3dbdf614a5ffbd23be557522cdf
Priyansh Garg <priyanshgarg709@gmail.com>
2023-03-29 13:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ if uploaded_file is not None:
   predictions = model.predict_proba(df)
   predictions = np.where(predictions[:, 1] >= 0.5, 1, 0)
   potability = pd.DataFrame(predictions, columns=['Potability'])
-  potability[potability][0]=0
+  potability.loc[0]=0
+  potability.loc[1]=0
   fig = px.scatter(df, x="ph", y="Turbidity", template="plotly_dark")
   st.write(fig)
   st.write(potability)
