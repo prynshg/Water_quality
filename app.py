adcfe805960c6f7cdc2052ee58f5fa8e6971b63d
Priyansh Garg <priyanshgarg709@gmail.com>
2023-03-31 14:09:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ if uploaded_file is not None:
   potability.loc[1]=0
   fig = px.scatter(df, x="ph", y="Turbidity", template="plotly_dark")
   st.write(fig)
-  st.write("Prediction of potability using the values of TDS and Turbidity")
+  st.write("Prediction of potability using ")
+  st.write("the values of TDS and Turbidity")
   st.write(potability)
   st.write("Output")
   if (potability.iloc[0].max() == 1):
